51228962fcdcc1031c3e4d07c2ce56c5e824ba29
Daniel Vena <deaconvena+github@gmail.com>
2019-10-18 10:54:41 -0400

    fix plugin info

--- plugin.php
+++ plugin.php
@@ -1,12 +1,13 @@
 <?php
 /**
  * Plugin Name: CardFlip Block
- * Plugin URI: https://github.com/cumulus-media/gutenberg-imageflip/
+ * Plugin URI: https://github.com/cumulus-digital/gutenberg-imageflip/
  * Description: Image flip card block for Wordpress' Gutenberg
  * Author: vena
  * Version: 1.0.1
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+ * GitHub Plugin URI: cumulus-digital/gutenberg-imageflip
  *
  * @package CGB
  */
