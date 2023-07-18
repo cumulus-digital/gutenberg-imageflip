a4e3669e72882b1389c8c69983ed261daf340b80
Daniel Vena <deaconvena+github@gmail.com>
2023-07-18 16:56:11 -0400

    package update

--- build/backend.asset.php
+++ build/backend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-element'), 'version' => '405ddfc59a26ca347962');
+<?php return array('dependencies' => array('lodash', 'wp-element'), 'version' => 'fa654c0dd999ab742e95');
